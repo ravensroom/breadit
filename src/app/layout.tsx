8149e5711a22739dd41eb79d424a194f73638238
raven <raven.lin.010@gmail.com>
2023-07-15 04:19:24 -0700

    ui: (auth)-sign-in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,36 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { Inter } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import Navbar from '@/components/Navbar';
+import { Toaster } from '@/components/ui/Toaster';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Breadit',
   description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        'bg-white text-slate-900 antialiased light',
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          <Navbar />
+          {children}
+        </div>
+        <Toaster />
+      </body>
     </html>
-  )
+  );
 }

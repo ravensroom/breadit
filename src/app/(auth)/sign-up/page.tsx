0f623cfeed046908c7e34a2f814fb71215499c0a
raven <raven.lin.010@gmail.com>
2023-07-20 01:51:29 -0700

    auth; route interception with modal

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,10 +1,13 @@
-import SignIn from '@/components/auth/SignIn';
+import SignUp from '@/components/auth/SignUp';
 import { buttonVariants } from '@/components/ui/Button';
 import { cn } from '@/lib/utils';
 import { ChevronLeft } from 'lucide-react';
 import Link from 'next/link';
+import { FC } from 'react';
 
-const page = ({}) => {
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
   return (
     <div className="absolute inset-0">
       <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
@@ -19,7 +22,7 @@ const page = ({}) => {
           Home
         </Link>
 
-        <SignIn />
+        <SignUp />
       </div>
     </div>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,10 @@ export const metadata = {
 
 export default function RootLayout({
   children,
+  authModal,
 }: {
   children: React.ReactNode;
+  authModal: React.ReactNode;
 }) {
   return (
     <html
@@ -26,8 +28,13 @@ export default function RootLayout({
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
         <div className="container max-w-7xl mx-auto h-full pt-12">
+          {/* @ts-expect-error server component */}
           <Navbar />
-          {children}
+          {authModal}
+
+          <div className="container max-w-7xl mx-auto h-full pt-12">
+            {children}
+          </div>
         </div>
         <Toaster />
       </body>

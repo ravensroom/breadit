672fc1d5cf4e5f222d835267849b84c1b3c505ec
raven <raven.lin.010@gmail.com>
2023-07-24 12:08:43 -0700

    comment vote: page + api

--- src/components/comment/PostComment.tsx
+++ src/components/comment/PostComment.tsx
@@ -1,9 +1,21 @@
 'use client';
 
+import { useOnClickOutside } from '@/hooks/use-on-click-outside';
 import { formatTimeToNow } from '@/lib/utils';
-import { CommentVote, User, Comment } from '@prisma/client';
-import { useRef } from 'react';
+import { CommentRequest } from '@/lib/validators/comment';
+import { Comment, CommentVote, User } from '@prisma/client';
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+import { MessageSquare } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { FC, useRef, useState } from 'react';
+import CommentVotes from './CommentVotes';
 import UserAvatar from '../user/UserAvatar';
+import { Button } from '../ui/Button';
+import { Label } from '../ui/Label';
+import { Textarea } from '../ui/Textarea';
+import { toast } from '../../hooks/use-toast';
+import { useSession } from 'next-auth/react';
 
 type ExtendedComment = Comment & {
   votes: CommentVote[];
@@ -12,10 +24,49 @@ type ExtendedComment = Comment & {
 
 interface PostCommentProps {
   comment: ExtendedComment;
+  votesAmt: number;
+  currentVote: CommentVote | undefined;
+  postId: string;
 }
 
-const PostComment: React.FC<PostCommentProps> = ({ comment }) => {
+const PostComment: FC<PostCommentProps> = ({
+  comment,
+  votesAmt,
+  currentVote,
+  postId,
+}) => {
+  const { data: session } = useSession();
+  const [isReplying, setIsReplying] = useState<boolean>(false);
   const commentRef = useRef<HTMLDivElement>(null);
+  const [input, setInput] = useState<string>(`@${comment.author.username} `);
+  const router = useRouter();
+  useOnClickOutside(commentRef, () => {
+    setIsReplying(false);
+  });
+
+  const { mutate: postComment, isLoading } = useMutation({
+    mutationFn: async ({ postId, text, replyToId }: CommentRequest) => {
+      const payload: CommentRequest = { postId, text, replyToId };
+
+      const { data } = await axios.patch(
+        `/api/subreddit/post/comment/`,
+        payload
+      );
+      return data;
+    },
+
+    onError: () => {
+      return toast({
+        title: 'Something went wrong.',
+        description: "Comment wasn't created successfully. Please try again.",
+        variant: 'destructive',
+      });
+    },
+    onSuccess: () => {
+      router.refresh();
+      setIsReplying(false);
+    },
+  });
 
   return (
     <div ref={commentRef} className="flex flex-col">
@@ -39,6 +90,71 @@ const PostComment: React.FC<PostCommentProps> = ({ comment }) => {
       </div>
 
       <p className="text-sm text-zinc-900 mt-2">{comment.text}</p>
+
+      <div className="flex gap-2 items-center">
+        <CommentVotes
+          commentId={comment.id}
+          votesAmt={votesAmt}
+          currentVote={currentVote}
+        />
+
+        <Button
+          onClick={() => {
+            if (!session) return router.push('/sign-in');
+            setIsReplying(true);
+          }}
+          variant="ghost"
+          size="xs"
+        >
+          <MessageSquare className="h-4 w-4 mr-1.5" />
+          Reply
+        </Button>
+      </div>
+
+      {isReplying ? (
+        <div className="grid w-full gap-1.5">
+          <Label htmlFor="comment">Your comment</Label>
+          <div className="mt-2">
+            <Textarea
+              onFocus={(e) =>
+                e.currentTarget.setSelectionRange(
+                  e.currentTarget.value.length,
+                  e.currentTarget.value.length
+                )
+              }
+              autoFocus
+              id="comment"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              rows={1}
+              placeholder="What are your thoughts?"
+            />
+
+            <div className="mt-2 flex justify-end gap-2">
+              <Button
+                tabIndex={-1}
+                variant="subtle"
+                onClick={() => setIsReplying(false)}
+              >
+                Cancel
+              </Button>
+              <Button
+                isLoading={isLoading}
+                onClick={() => {
+                  if (!input) return;
+                  postComment({
+                    postId,
+                    text: input,
+                    replyToId: comment.replyToId ?? comment.id, // default to top-level comment
+                  });
+                }}
+              >
+                Post
+              </Button>
+            </div>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
